c37334eed7bef3db1a33183c7c5f3227e7b5e126
Sifei Liu <zwrq0star@gmail.com>
2017-08-14 01:15:16 -0700

    update datalayers
    add roi-related datalayer and utils

---
+++ pylayers/data_roidb.py
@@ -0,0 +1,62 @@
+import caffe
+import numpy as np
+import math
+from PIL import Image
+import scipy.io
+import random
+from pyutils import datalayer_util as du
+
+def load_imagelabel_ac(voc_dir, idx, shape, mean_):
+	width_ = np.array(1.2*shape[3],dtype=int)
+	height_ = np.array(1.2*shape[2],dtype=int)
+	im = Image.open('{}/JPEGImages/{}.jpg'.format(voc_dir, idx))
+	lb = Image.open('{}/SegmentationClass/{}.png'.format(voc_dir, idx))
+	(x,y) = im.size
+	center = (x/2,y/2)
+	rate = (np.random.rand(1)-0.5)
+	shift_x = np.maximum(x,y) * rate
+	rate = (np.random.rand(1)-0.5)
+	shift_y = np.maximum(x,y) * rate
+	scale_x = 1+(np.random.rand(1)-0.5) /5.0
+	scale_y = 1+(np.random.rand(1)-0.5) /5.0
+	angle = (np.random.rand(1)-0.5)*(30.0/180.0)*math.pi
+	mat = du.Affinemat(angle,scale_x,scale_y,center,(center[0]+shift_x,center[1]+shift_y))
+	im = im.transform((x,y), Image.AFFINE, mat, resample=Image.BILINEAR)
+	lb = lb.transform((x,y), Image.AFFINE, mat, resample=Image.BILINEAR)
+	image = np.array(im,dtype=np.float32)
+	image = image[:,:,::-1]
+	image -= mean_
+	label = np.array(lb,dtype=np.uint8)
+	label = label[:,:,np.newaxis]
+
+    image = image.transpose((2,0,1))
+    label = label.transpose((2,0,1))
+	  
+	return image, label
+
+
+def getrois(label, roi_num, roi_dim):
+	# image: h*w*k
+	# images: h*w*k*n
+	# sample according to the pretrained model
+	im_shape = np.array(label.shape)
+	count = 0
+	protect = 0
+	rois = np.zeros((roi_num,5))
+	if len(np.unique(label[label<21])) < 2:
+		mmp = 0
+	else:
+		mmp = 1
+
+	while count < roi_num:
+		top = int(np.random.rand(1)* (im_shape[0]-roi_dim[0]))
+		left = int(np.random.rand(1)* (im_shape[1]-roi_dim[1]))
+		tmp_lb = label[0,top:top+roi_dim[0],left:left+roi_dim[1]]
+		nidx = np.unique(tmp_lb[tmp_lb<21])
+		if (len(nidx) < 2 and mmp) and protect < 10:
+			protect += 1
+			continue
+		protect = 0
+		rois[count] = np.array((left,top,roi_dim[0],roi_dim[1]))
+		count += 1
+	return rois
---
+++ pylayers/roisample_layer.py
@@ -0,0 +1,88 @@
+import caffe
+import numpy as np
+import math
+from PIL import Image
+import scipy.io
+import random
+import sys
+from pyutils import data_roidb as db_roi
+# from multiprocessing import Process, Queue
+
+
+class ROISampleDataLayer(caffe.Layer):
+	"""
+	Load (input image, cnn output, label image) pairs from PASCAL VOC
+	one-at-a-time while reshaping the net to preserve dimensions.
+
+	Use this to feed data to a rnn refine network with multiple label at once.
+	"""
+
+	def setup(self, bottom, top):
+		"""
+		roi_dim: roi width/height, usually set as 128; need to copy to roi layer param
+		"""
+		# config
+		params = eval(self.param_str)
+		self.USE_PREFETCH = params.get('USE_PREFETCH',False)
+		self.root_dir = params['root']
+		self.split = params['split']
+		self.mean = np.array(params['mean'])
+		self.random = params.get('randomize', True)
+		self.seed = params.get('seed', None)
+		self.shape = np.array(params['shape'])
+		self.imagenorm = params.get('norm', False)
+		self.roi_num = params['roi_num_per_im']
+		self.roi_dim = params['roi_dim']
+
+		self.sample_size = self.shape[0] / self.roi_num
+
+		if len(top) != 3:
+			raise Exception("Need to define three tops: data, label and rois.")
+		if len(bottom) != 0:
+			raise Exception("Do not define a bottom.")
+
+		# load indices for images and labels
+		split_f  = '{}/ImageSets/Segmentation/{}.txt'.format(self.voc_dir,
+				self.split)
+		self.indices = open(split_f, 'r').read().splitlines()
+		self.idx = np.array(range(self.sample_size))
+
+		random.seed(self.seed)
+		for id in range(len(self.idx)):
+			self.idx[id] = random.randint(0, len(self.indices)-1)
+
+
+	def reshape(self, bottom, top):
+		self.data = np.zeros(self.shape)
+		self.label = np.zeros((self.shape[0],1,self.shape[2],self.shape[3]))
+		self.rois = np.zeros((self.roi_num,5))
+
+		# reshape tops to fit (leading 1 is for batch dimension)
+		top[0].reshape(*self.data.shape)
+		top[1].reshape(*self.label.shape)
+		top[2].reshape(*self.rois)
+
+	def get_next_minibatch_idx(self):
+		for id in self.sample_size:
+			idx[id] = random.randint(0, len(self.indices)-1)
+		return idx
+
+	def get_next_minibatch(self):
+		"""return everything"""
+		idx_ = get_next_minibatch_idx()
+		for id in self.sample_size:
+			image[id], label[id]= \
+				db_roi.load_imagelabel_ac(self.indices[idx_[id]],self.shape)
+			rois[id*self.roi_num : (id+1)*self.roi_num] = \
+				db_roi.getrois(label, self.roi_num, self.roi_dim)
+
+	
+	def forward(self, bottom, top):
+		get_next_minibatch()
+        top[0].data[...] = self.data
+        top[1].data[...] = self.label
+        top[2].data[...] = self.rois
+
+    
+    def backward(self, top, propagate_down, bottom):
+        pass
